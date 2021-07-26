c6dd603f07ad33a9173615e070b58e1fb2e863f5
Kolya Shevelinda <kolyashevelinda@gmail.com>
2021-07-26 15:37:06 +0300

    create function generators

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -16,6 +16,23 @@ export class CardComponent implements OnInit {
     imgUrl: string = 'https://angular.io/assets/images/logos/angular/angular.png';
 
     ngOnInit() {
+        // function countImg1() {
+        //     let count1:number;
+        //     if (count1) {
+        //         setTimeout(()=>{
+        //             this.imgUrl = 'https://www.dotcom-monitor.com/blog/wp-content/uploads/sites/3/2020/05/Vue-logo-1.png'
+        //         }, 3000) 
+        //     }
+        // }
+
+        // function countImg2(count2:number) {
+        //     if (count2) {
+        //         setTimeout(()=>{
+        //             this.imgUrl = 'https://angular.io/assets/images/logos/angular/angular.png'
+        //         }, 6000)
+        //     }
+        // }
+
         setTimeout(()=>{
             this.imgUrl = 'https://www.dotcom-monitor.com/blog/wp-content/uploads/sites/3/2020/05/Vue-logo-1.png'
         }, 3000)
